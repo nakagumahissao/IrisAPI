40c52dbc5ef0e2a601d6eaa77b3dc9fa6b050235
Sandro Hissao Nakaguma <nakagumahissao@outlook.com>
2025-08-27 10:19:55 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+
+namespace IrisAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapGet("/iris", (float sepalLength, float sepalWidth, float petalLength, float petalWidth) =>
+            {
+                var sampleData = new MLIrisModel.ModelInput()
+                {
+                    Sepal_Length = sepalLength,
+                    Sepal_Width = sepalWidth,
+                    Petal_Length = petalLength,
+                    Petal_Width = petalWidth,
+                };
+
+                var result = MLIrisModel.Predict(sampleData);
+
+                var species = result.PredictedLabel.Replace(" ", "").ToLower().Replace("-", ""); // make filename safe
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "img", $"{species}.jpeg");
+
+                string? base64Image = null;
+                if (File.Exists(filePath))
+                {
+                    var bytes = File.ReadAllBytes(filePath);
+                    base64Image = $"data:image/png;base64,{Convert.ToBase64String(bytes)}";
+                }
+
+                return Results.Json(new
+                {
+                    SepalLength = sepalLength,
+                    SepalWidth = sepalWidth,
+                    PetalLength = petalLength,
+                    PetalWidth = petalWidth,
+                    PredictedSpecies = result.PredictedLabel,
+                    ImageBase64 = base64Image
+                });
+            })
+            .WithName("GetIris")
+            .WithOpenApi();
+
+            app.Run();
+        }
+    }
+}
